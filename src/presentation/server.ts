4f6c1cdd17c40cbab4115ea4924e6778a12f233f
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-03-06 22:33:52 +0100

    feat: add check-service.ts - use case and presentations

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,19 @@
+/* Use cases */
+import { CheckService } from "../domain/use-cases/checks/check-service";
+
+/* Presentations */
+import { CronService } from "./cron/cron-service";
+
 export class Server {
   public static start() {
     console.log("Server started...");
+    const url = "https://google.com";
+
+    CronService.createJob("*/5 * * * * *", () => {
+      new CheckService(
+        () => console.log(`${url} is ok`),
+        (error) => console.log(error)
+      ).execute(url);
+    });
   }
 }

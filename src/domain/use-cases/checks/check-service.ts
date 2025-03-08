e61dfd37d3ac109a05e4c7bc5eba72543f0ee0b1
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-03-08 19:46:19 +0100

    feat: update server.ts with a file system repository

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,15 +1,27 @@
+/* Entities */
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+
+/* Repository */
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
   private readonly successCallback: SuccessCallback;
   private readonly errorCallback: ErrorCallback;
+  private readonly logRepository: LogRepository;
 
-  constructor(successCallback: SuccessCallback, errorCallback: ErrorCallback) {
+  constructor(
+    logRepository: LogRepository,
+    successCallback: SuccessCallback,
+    errorCallback: ErrorCallback
+  ) {
+    this.logRepository = logRepository;
     this.successCallback = successCallback;
     this.errorCallback = errorCallback;
   }
@@ -20,11 +32,20 @@ export class CheckService implements CheckServiceUseCase {
 
       if (!req.ok) throw new Error(`Error on check service ${url}`);
 
-      this.successCallback();
+      const log = new LogEntity(
+        `Service ${url}, working`,
+        LogSeverityLevel.LOW
+      );
+      this.logRepository.saveLog(log);
+
+      this.successCallback && this.successCallback();
       return true;
     } catch (error) {
-      this.errorCallback(`${error}`);
-      console.log(`${error}`);
+      const errorMessage = `${url} is not ok. ${error}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.HIGH);
+      this.logRepository.saveLog(log);
+
+      this.errorCallback && this.errorCallback(`${errorMessage}`);
 
       return false;
     }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,18 @@
-/* Use cases */
+/* Use cases - checks */
 import { CheckService } from "../domain/use-cases/checks/check-service";
+
+/* Infrastructure - datasources */
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+/* Infrastructure - repositories */
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 
 /* Presentations */
 import { CronService } from "./cron/cron-service";
+
+// Variable to change the repositories
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+);
 
 export class Server {
   public static start() {
@@ -11,9 +21,12 @@ export class Server {
 
     CronService.createJob("*/5 * * * * *", () => {
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} is ok`),
         (error) => console.log(error)
       ).execute(url);
+
+      // new CheckService().execute("http://localhost:3000")
     });
   }
 }

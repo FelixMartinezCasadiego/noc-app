cdaa3a4e6c065ffd922d78013488b6400323ca67
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-03-19 20:26:05 +0100

    test: add docker compose to test and create envs.plugin.test.ts

---
+++ test/src/configs/plugins/envs.plugin.test.ts
@@ -0,0 +1,29 @@
+import { envs } from "../../../../src/configs/plugins/envs.plugin";
+
+describe("EnvsPlugin", () => {
+  test("should return env options", () => {
+    expect(envs).toEqual({
+      PORT: 3000,
+      MAILER_EMAIL: "felix.martinezcasadiego@gmail.com",
+      MAILER_SECRET_KEY: "zosvhgdxmtknzvyd",
+      MAILER_SERVICE: "gmail",
+      MONGO_URL: "mongodb://felix:123456@localhost:27017/",
+      MONGO_DB_NAME: "NOC-TEST",
+      MONGO_USER: "felix",
+      MONGO_PASS: "123456789",
+    });
+  });
+
+  test("should return error if not found env", async () => {
+    jest.resetModules();
+    process.env.PORT = "ABC";
+
+    try {
+      await import("../../../../src/configs/plugins/envs.plugin");
+
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(`${error}`).toContain('"PORT" should be a valid integer');
+    }
+  });
+});
